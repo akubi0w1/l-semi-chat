524311a2c420513983284767de6d1b5aa6713fe4
AkariKanaya <akari317017@gmail.com>
2020-01-09 15:50:56 +0900

    update1

--- mysql/init.d/dml.sql
+++ mysql/init.d/dml.sql
@@ -3,7 +3,8 @@
 
 
 -- threads
-
+INSERT INTO `ls_chat`.`threads`(`id`,`name`,`description`,`limit_users`,`user_id`,`is_public`,`created_at`,`updated_at`)  VALUES ("11111111-1111-1111-1111-111111111111",”name1”,”description1”,1,”11111111-1111-1111-1111-111111111111”,1,cast('2018-11-24 11:56:40' as datetime),cast('2018-11-24 11:56:40' as datetime));
+INSERT INTO `ls_chat`.`threads`(`id`,`name`,`user_id`) VALUES ("22222222-2222-2222-2222-222222222222”,”name1”,”22222222-2222-2222-2222-222222222222”);
 
 -- messages
 
@@ -25,15 +26,21 @@
 
 -- users_followers
 
+INSERT INTO `ls_chat`.`users_followers`(`id`,`user_id`,`followed_user_id`) VALUES (“11111111-1111-1111-1111-111111111111”,“11111111-1111-1111-1111-111111111111”,“11111111-1111-1111-1111-111111111111”);
 
 -- users_tags
 
 
 -- users_threads
 
+INSERT INTO `ls_chat`.`users_threads`(`id`,`user_id`,`thread_id`,`is_admin`) VALUES ("11111111-1111-1111-1111-111111111111","11111111-1111-1111-1111-111111111111","11111111-1111-1111-1111-111111111111",1);
+INSERT INTO `ls_chat`.`users_threads`(`id`,`user_id`,`thread_id`) VALUES ("22222222-2222-2222-2222-222222222222","22222222-2222-2222-2222-222222222222","22222222-2222-2222-2222-222222222222");
 
 -- users_favorites
 
 
 -- threads_tags
+
+INSERT INTO `ls_chat`.`users_followers`(`id`,`thread_id`,`tag_id`) VALUES (“11111111-1111-1111-1111-111111111111”,“11111111-1111-1111-1111-111111111111”,“11111111-1111-1111-1111-111111111111”);
+
 
